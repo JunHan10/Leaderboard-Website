e10bfee9a16adab7c8f6a23e7810698e362de916
Jun Han <142513414+JunHan10@users.noreply.github.com>
2025-06-06 22:45:27 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,18 @@
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDZPo_KulnDOOXa_bom1e2xxaDn2rbaQEI",
-        authDomain: "red-team-leaderboard.firebaseapp.com",
-        databaseURL: "https://red-team-leaderboard-default-rtdb.firebaseio.com",
-        projectId: "red-team-leaderboard",
-        storageBucket: "red-team-leaderboard.firebasestorage.app",
-        messagingSenderId: "739501164037",
-        appId: "1:739501164037:web:a1ba49246f4c3ea59ffe6c",
-        measurementId: "G-F6TMV87FT2"
+    authDomain: "red-team-leaderboard.firebaseapp.com",
+    databaseURL: "https://red-team-leaderboard-default-rtdb.firebaseio.com",
+    projectId: "red-team-leaderboard",
+    storageBucket: "red-team-leaderboard.firebasestorage.app",
+    messagingSenderId: "739501164037",
+    appId: "1:739501164037:web:a1ba49246f4c3ea59ffe6c",
+    measurementId: "G-F6TMV87FT2"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const database = firebase.database(); 
+const database = firebase.database();
+
+// Enable Firebase Analytics
+firebase.analytics(); 
